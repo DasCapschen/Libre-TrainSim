9435deb0c419fcd16e54b297c0c238acf21ff27b
DasCapschen <3923406+DasCapschen@users.noreply.github.com>
2021-10-09 12:32:03 +0200

    fix distance_on_route

--- src/addons/Libre_Train_Sim_Editor/Data/Scripts/DistanceMonitor.gd
+++ src/addons/Libre_Train_Sim_Editor/Data/Scripts/DistanceMonitor.gd
@@ -16,7 +16,7 @@ func set_player(player):
 	self._player = player
 
 func distance_left():
-	return (_start_dist + _distance) - _player.distance
+	return (_start_dist + _distance) - _player.distance_on_route
 
 func _ready():
 	self.set_process(false)
@@ -26,7 +26,7 @@ func start():
 	if _player == null:
 		printerr("DistanceMonitor: Need to set_player() before calling start()!")
 		return
-	self._start_dist = _player.distance
+	self._start_dist = _player.distance_on_route
 	self.is_stopped = false
 	self.set_process(true)
 
@@ -35,6 +35,6 @@ func stop():
 	self.is_stopped = true
 
 func _process(delta):
-	if _player.distance > _start_dist + _distance:
+	if _player.distance_on_route > _start_dist + _distance:
 		emit_signal("reached")
 		stop()
